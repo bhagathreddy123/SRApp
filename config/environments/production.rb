d4fe0508c0b5d9fa25e4f18f0fc5677fd61a0690
rails <rails@rails.com>
2015-01-27 11:22:57 +0530

    add appname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   # config.autoflush_log = false
  config.active_support.deprecation = :notify
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'http://SRapp.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://srapp.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     :address => 'smtp.gmail.com',
     #    :domain => 'gmail.com',
